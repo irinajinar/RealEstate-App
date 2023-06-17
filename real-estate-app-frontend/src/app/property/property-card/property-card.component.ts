3eeb9cfceef5e8c4f4a7bc820c9f850e6e1aec9b
Irina Jinar <irinajinar.simedru@gmail.com>
2023-06-17 10:40:47 +0300

    added HTTP requests

--- real-estate-app-frontend/src/app/property/property-card/property-card.component.ts
+++ real-estate-app-frontend/src/app/property/property-card/property-card.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input } from "@angular/core";
+import { IProperty } from "../IProperty.interface";
 
 @Component({
   selector: 'app-property-card',
-  // template: `<h1>I am a card</h1>`,
   templateUrl: 'property-card.component.html',
-  // styles: ['h1 {font-weight: normal;}']
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent {
-  @Input() property : any
- 
-
+  @Input() property!: IProperty;
 }
